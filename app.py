af4ca79ac4b17e75541ecaed1672575e463c08af
amaki <in345vain@gmail.com>
2021-07-22 11:32:10 +0900

    initial commit

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request
-from wtforms import StringField, SubmitField, IntegerField
-from wtforms.form import Form
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY']
